2711ff2b704d455f2448f4f61c11a422e579a901
unknown <jorgesosa@fundaciondevalores.org>
2024-03-11 17:59:54 -0400

    refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,81 +1,83 @@
 const left_list = document.querySelector('#left_list')
+const toRight = document.querySelector('#toRight')
+const allToRight = document.querySelector('#allToRight')
+const toLeft = document.querySelector('#toLeft')
+const allToLeft = document.querySelector('#allToLeft')
 const right_list = document.querySelector('#right_list')
 const new_item = document.querySelector('#new_item')
 const add = document.querySelector('#add')
-const toLeft = document.querySelector('#to_left')
 
 const miLista = [
-    { id: 1, texto: '1', isLeft: true },
-    { id: 2, texto: '2', isLeft: false },
-    { id: 3, texto: '3', isLeft: true },
-
-
+    { id: 1, texto: "Jorge", isLeft: true },
+    { id: 2, texto: "Daniel", isLeft: false },
+    { id: 3, texto: "Jose", isLeft: false },
+    { id: 4, texto: "Manuel", isLeft: true },
 ]
 
-function innerHtmlElment() {
-    miLista.forEach(ele => {
-        const item = `
-            <li>
-                <input type="checkbox" id="${ele.id}" />
-                <label for="${ele.id}">${ele.texto}</label>
-            </li>`
+let move = []
 
-        if (ele.isLeft) {
-            left_list.innerHTML += item
+creteDomElment()
 
-        } else {
-            right_list.innerHTML += item
-        }
-
-    })
-
-    const inputList = [...document.querySelectorAll('input[type=checkbox]')]
-    inputList.forEach(item => item.addEventListener('change', function () {
-        console.log(this.id)
-    }))
+function addItem(texto) {
+    if (texto.value != '') {
+        miLista.push({
+            id: miLista.length + 1,
+            texto: texto.value,
+            isLeft: true
+        })
+        texto.value = ''
+        creteDomElment()
+    }
 }
 
+function creteDomElment() {
+    left_list.innerHTML = ''
+    right_list.innerHTML = ''
 
-function domElment() {
     miLista.forEach(el => {
-        const li = document.createElement('li')
+        const li = document.createElement('li');
         const input = document.createElement('input')
         const label = document.createElement('label')
 
+        input.id = el.id;
         input.type = 'checkbox'
-        input.id = el.id
-        label.htmlFor = el.id
-        label.textContent = el.texto
-        input.addEventListener('change', function(){
-            console.log(this)
+
+        input.addEventListener('change', function () {
+            move.push(parseInt(input.id))
         })
+
+        label.htmlFor = el.id;
+        label.textContent = el.texto
+
         li.append(input, label)
 
-            if(el.isLeft){
-                left_list.appendChild(li)
-            }else{
-                right_list.appendChild(li)
+        if (el.isLeft) {
+            left_list.appendChild(li)
+        } else {
+            right_list.appendChild(li)
+        }
+    });
+}
+
+add.addEventListener('click', () => addItem(new_item))
+
+toRight.addEventListener('click', () =>  validation(false))
+
+toLeft.addEventListener('click', () => validation(true))
+
+function validation(val) {
+  
+    move.forEach(id => {
+
+        miLista.forEach(item => {
+            if (item.id === id) {
+                item.isLeft =  val
             }
-        
-    })
+
+        });
+
+    });
+
+    creteDomElment()
+    move = []
 }
-
-// innerHtmlElment()
-
-domElment()
-
-function addNewItem(text) {
-    miLista.push({
-        id: miLista.length + 1,
-        texto: text,
-        isLeft: true
-    })
-    left_list.innerHTML = ''
-
-    domElment()
-}
-
-add.addEventListener('click', () => addNewItem(new_item.value))
-/* toLeft.addEventListener('click', function() {
-
-}) */
