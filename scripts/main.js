6c6bcac109d55941cfb606d8ff9ed53599d89da2
unknown <jorgesosa@fundaciondevalores.org>
2024-03-11 12:04:34 -0400

    Dom ELment evets

--- scripts/main.js
+++ scripts/main.js
@@ -20,39 +20,49 @@ function innerHtmlElment() {
                 <label for="${ele.id}">${ele.texto}</label>
             </li>`
 
-            if(ele.isLeft) {
-                left_list.innerHTML += item
-                
-            }else{
-                right_list.innerHTML += item
-            }
-                
+        if (ele.isLeft) {
+            left_list.innerHTML += item
+
+        } else {
+            right_list.innerHTML += item
+        }
+
     })
 
     const inputList = [...document.querySelectorAll('input[type=checkbox]')]
-    inputList.forEach(item => item.addEventListener('change', function() {
-         console.log(this.id)
+    inputList.forEach(item => item.addEventListener('change', function () {
+        console.log(this.id)
     }))
 }
 
 
-function domElment(id, texto, element) {
-    const li = document.createElement('li')
-    const input = document.createElement('input')
-    const label = document.createElement('label')
- 
-    input.type = 'checkbox'
-    input.id = id
-    label.htmlFor = id
-    label.textContent = texto
+function domElment() {
+    miLista.forEach(el => {
+        const li = document.createElement('li')
+        const input = document.createElement('input')
+        const label = document.createElement('label')
 
-    li.append(input, label)
+        input.type = 'checkbox'
+        input.id = el.id
+        label.htmlFor = el.id
+        label.textContent = el.texto
+        input.addEventListener('change', function(){
+            console.log(this)
+        })
+        li.append(input, label)
 
-    element.appendChild(li)
-
+            if(el.isLeft){
+                left_list.appendChild(li)
+            }else{
+                right_list.appendChild(li)
+            }
+        
+    })
 }
 
-innerHtmlElment(left_list)
+// innerHtmlElment()
+
+domElment()
 
 function addNewItem(text) {
     miLista.push({
@@ -62,7 +72,7 @@ function addNewItem(text) {
     })
     left_list.innerHTML = ''
 
-    innerHtmlElment(left_list)
+    domElment()
 }
 
 add.addEventListener('click', () => addNewItem(new_item.value))
